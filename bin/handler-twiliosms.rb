8eb4e75778f9b3588cc2673c4124c92448c79ec1
Jef Spaleta <jef@sensu.io>
2018-12-27 15:39:53 -0900

    refactor to make rubocup happy

--- bin/handler-twiliosms.rb
+++ bin/handler-twiliosms.rb
@@ -27,19 +27,50 @@ class TwilioSMS < Sensu::Handler
          boolean: true,
          default: false
 
-
   def short_name
-    (@event['client']['name'] || 'unknown') + '/' +(@event['check']['name'] || 'unknown')
+    (@event['client']['name'] || 'unknown') + '/' + (@event['check']['name'] || 'unknown')
+  end
+
+  def output
+    (@event['check']['output'] || 'no check output')
+  end
+
+  def address
+    (@event['client']['address'] || 'unknown address')
+  end
+
+  def check_name
+    @event['check']['name'] if @event.include? 'check'
+  end
+
+  def check_status
+    check_status = @event['check']['status'] if @event.include? 'check'
+    check_status || -1
   end
 
   def action_to_string
     @event['action'].eql?('resolve') ? 'RESOLVED' : 'ALERT'
   end
 
+  def match(candidate)
+    matching = false
+    sensu_role_all = candidate['sensu_roles'].include?('all')
+    puts " sensu_roles includes all: #{sensu_role_all}" if config[:verbose]
+    matching ||= sensu_role_all
+    keepalive_check = @event['check']['name'].eql?('keepalive')
+    sensu_role_keepalive = candidate['sensu_roles'].include?('keepalive')
+    puts " matching keepalive: #{(sensu_role_keepalive && keepalive_check)}" if config[:verbose]
+    matching ||= (sensu_role_keepalive && keepalive_check)
+    matching_subscribers = (@event['check']['subscribers'] &&
+          (candidate['sensu_roles'] & @event['check']['subscribers']).size > 0)
+    puts " matching_subscribers: #{matching_subscribers}" if config[:verbose]
+    matching ||= matching_subscribers
+    matching_checkname = candidate['sensu_checks'].include?(check_name)
+    puts " matching_checkname: #{matching_checkname}" if config[:verbose]
+    matching || matching_checkname
+  end
+
   def handle
-    check_status = @event['check']['status'] if @event.include? 'check'
-    check_status ||= -1
-    check_name = @event['check']['name'] if @event.include? 'check'
     account_sid = settings['twiliosms']['sid']
     auth_token = settings['twiliosms']['token']
     from_number = settings['twiliosms']['number']
@@ -56,37 +87,24 @@ class TwilioSMS < Sensu::Handler
     puts "Checkname: #{check_name}" if config[:verbpse]
     candidates.each do |mobile, candidate|
       puts "matching conditions for #{mobile}:" if config[:verbose]
-      matching = false
-      sensu_role_all= candidate['sensu_roles'].include?('all')
-      puts " sensu_roles includes all: #{sensu_role_all}" if config[:verbose]
-      matching ||= sensu_role_all 
-      keepalive_check=@event['check']['name'].eql?('keepalive') 
-      sensu_role_keepalive=candidate['sensu_roles'].include?('keepalive')
-      puts " matching keepalive: #{ sensu_role_keepalive && keepalive_check}" if config[:verbose]
-      matching ||= ( sensu_role_keepalive && keepalive_check )   
-      matching_subscribers= (@event['check']['subscribers'] &&
-            (candidate['sensu_roles'] & @event['check']['subscribers']).size > 0)
-      puts " matching_subscribers: #{matching_subscribers}" if config[:verbose]
-      matching ||= matching_subscribers 
-      matching_checkname = candidate['sensu_checks'].include?(check_name)
-      puts " matching_checkname: #{matching_checkname}" if config[:verbose] 
-      matching ||= matching_checkname 
-      next unless matching  && ( candidate['sensu_level'] >= check_status )
-        
+      next unless match(candidate) && (candidate['sensu_level'] >= check_status)
+
       puts "Send text to: #{mobile}" if config[:verbose]
       recipients << mobile
     end
     message = if short
-                "Sensu #{action_to_string}: #{@event['check']['output']}"
+                "Sensu #{action_to_string}: #{output}"
               else
-                "Sensu #{action_to_string}: #{short_name} (#{@event['client']['address']|| 'unknown address'}) #{@event['check']['output'] || 'no check output'}"
+                "Sensu #{action_to_string}: #{short_name} (#{address}) #{output}"
               end
 
     message[157..message.length] = '...' if message.length > 160
 
     twilio = Twilio::REST::Client.new(account_sid, auth_token)
     recipients.each do |recipient|
-      unless config[:disable_send]
+      if config[:disable_send]
+        puts "From: #{from_number} To: #{recipient} Body: #{message}"
+      else
         begin
           twilio.account.messages.create(
             from: from_number,
@@ -97,8 +115,6 @@ class TwilioSMS < Sensu::Handler
         rescue StandardErorr => e
           puts "Failure detected while using Twilio to notify on event: #{e.message}"
         end
-      else
-        puts "From: #{from_number} To: #{recipient} Body: #{message}"
       end
     end
   end
